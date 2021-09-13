814f1d8c8540bbdc228573eadad93735966dfc20
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-13 11:31:39 +0200

    flex

---
+++ Js/script.js
@@ -0,0 +1,115 @@
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+const icons = [
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'food'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'food'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'food'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'food'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'food'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'food'
+    },
+
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'beverage'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'beverage'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'beverage'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'beverage'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'beverage'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'beverage'
+    },
+
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'animal'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'animal'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'animal'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'animal'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'animal'
+    },
+    {
+        name: '',
+        family: 'fas',
+        prefix: 'fa-',
+        category: 'animal'
+    },
+];
